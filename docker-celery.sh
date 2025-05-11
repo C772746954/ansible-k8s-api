d13da8507f4353e2c86e55d81760d0d016acad0a
C772746954 <50334355+C772746954@users.noreply.github.com>
2025-05-11 15:16:00 +0800

    Delete docker-celery.sh
    1

--- docker-celery.sh
+++
@@ -1,8 +0,0 @@
-# 构建Celery镜像
-docker build -f Dockerfile.celery -t k8s-celery-worker:1.0 .
-
-# 查看Worker状态
-docker exec -it <container_id> celery -A app.celery.tasks status
-
-# 监控任务队列
-docker exec -it <container_id> celery -A app.celery.tasks inspect active
