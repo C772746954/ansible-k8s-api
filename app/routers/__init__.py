34467b0f9f6ef2bab05ed1c4f5b1f229cb95b368
C772746954 <50334355+C772746954@users.noreply.github.com>
2025-05-19 00:32:53 +0800

    Delete app directory
    1111

--- app/routers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .configmaps import router as configmaps_router
-from .auth import router as auth_router  # 其他路由按需添加
-
-__all__ = ["configmaps_router", "auth_router"]
--- app/secrets.py
+++
@@ -1,5 +0,0 @@
-import secrets
-
-# 生成 32 字节的随机字符串（可通过调整长度适应需求）
-secret_key = secrets.token_urlsafe(32)
-print(secret_key)
